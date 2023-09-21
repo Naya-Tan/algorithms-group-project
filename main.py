0fc2fdf3c80c60beb692a62bc18eeb192b041451
Naya-Tan <99350088+Naya-Tan@users.noreply.github.com>
2023-09-21 14:27:25 -0500

    updated io

--- main.py
+++ main.py
@@ -57,36 +57,50 @@ def gcd(a=1, b=1):
         return a
     else:
         return gcd(b, a%b)
-
-genKey()
    
-
-# methods for encryption and decryption 
-def encrypt(message, N, e): # you use public key for encryption 
-   # need to convert to ascii 
-   encrypted_message = pow(message, e, N)
-   # turn the message back into a string to return the message 
-   # when you encrypt the message it should be all integers 
+# functions to encrypt the message
+def encrypt_message(message, N, e): 
+   ''' Function uses public key to encrypt the message '''
+   encrypted_message = []
+   
+   for char in message: 
+      char = ord(char)
+      encrypted_char = encrypt(char, N, e)
+      encrypted_message.append(encrypted_char)
+      
    return encrypted_message
+
+def encrypt(char, N, e):
+   ''' encrypt each character'''
+   return pow(char, e, N)
+
+# functions to decrypt the message
+def decrypted_message(encrypted_message, N, d):
+   ''' Function uses private key to decrypt the encrypted message'''
+   message = []
    
-def decrypt(encypted_message, N, d): # use private key to decrypt
-   # comvert the encrypted message which is in all integers 
-   # back into ascii characters in order to return the original message 
-   decrypted_message = pow(encypted_message, d, N)
-   # turn it back into a string to return the message 
+   for char in encrypted_message:
+      decrypted_char = decrypt(char, N, d)
+      message.append(decrypted_char)
+      
+   decrypted_message = ''.join([chr(i) for i in message])
+  
    return decrypted_message
 
+def decrypt(char, N, d):
+   ''' decrypt each character'''
+   return pow(char, d, N)
+ 
 
 # methods for authentications 
-
-def sign(message, private_key): # sign a message using a private key
-   n, d = private_key
-   signature = pow(message, d, n)
+def sign(message, d, N): # sign a message using a private key
+   #n, d = private_key
+   signature = pow(message, d, N)
    return signature
 
-def verify(message, signature, public_key): # verify a signature using a public key
-   n, e = public_key
-   decrypted_signature = pow(signature, e, n)
+def verify(message, signature, N, e): # verify a signature using a public key
+   #n, e = public_key
+   decrypted_signature = pow(signature, e, N)
    return decrypted_signature == message
 
 
@@ -97,6 +111,8 @@ signatures = []
 
 print("\nRSA keys have been generated.")
 # generate the RSA keys here 
+N, e, d = genKey()
+
 
 while True:
         print()
@@ -110,7 +126,9 @@ while True:
                   if(choice == "1"):
                      message = input("Enter a message: ")
                      # pass message into the encrypt function 
-                     # store the message in the list encrypted_messages.append(message)
+                     emessage = encrypt_message(message, N, e)
+                     # store the message in the list 
+                     encrypted_messages.append(emessage)
                      print("Message encrypted and sent")
                      print()
                   elif(choice == "2"):
@@ -119,14 +137,16 @@ while True:
                      print("There are no messages to authenticate")
                    else:
                       print("The following messages are available: ")
-                   # for i in range(len(signatures):
-                   #       print(i, ".", signatures[i], end = "\n")
-                      signature = sign(message, private_key)
-                      print("Signature: ", signature)           
-                     #valid = input("Enter your choice")
+                      for i in range(len(signatures)):
+                          print(i, ".", len(signatures[i]), end = "\n")
+                      #signature = sign(message, private_key)
+                      #print("Signature: ", signature)           
+                      valid = input("Enter your choice")
                      # checking to see if its valid or not
-                      verified = verify(message, signature, public_key)
-                      print("Verified: ", verified)
+                      #verified = verify(message, signature, public_key)
+                      #print("Verified: ", verified)
+                      if (valid <= len(signatures)):
+                         print("Signature is valid")
                                    
                       print()
                   else:
@@ -137,15 +157,16 @@ while True:
                    choice = input("As owners of the keys, what would you like to do?\n1. Decrypt a reciever message\n2. Digitally sign a message\n3. Show the keys\n4. Generate a new set of keys\n5. Exit\nEnter your choice:")
 
                    if (choice == "1"):
-                     # if len(encypted_messages) == 0:
-                     #     print("There are no messages to decrypt")
-                     # else:
-                     #    print("The following messages are available:")
-                     #    for i in range(len(encrypted_messages)):
-                     #        print(i, ".", encrypted_messages[i], end ="\n")
-                     #     valid = input("Enter your choice: ")
-                     #     # add checking to see if its valid or not later
-                     #     # print("Decrypted message: ", decrypt(encrypted_messages[int(valid)]))
+                     if len(encrypted_messages) == 0:
+                         print("There are no messages to decrypt")
+                     else:
+                        print("The following messages are available:")
+                        for i in range(len(encrypted_messages)):
+                             print(i + 1, ". (length =", len(encrypted_messages[i]), ')', end ="\n")
+                        valid = input("Enter your choice: ")
+                        # add checking to see if its valid or not later
+                        if (int(valid) <= len(encrypted_messages)):
+                            print("Decrypted message:", decrypted_message(encrypted_messages[int(valid) -1 ], N, d))
                      print()
                    elif (choice == "2"):
                      signature = input("Enter signature: ")
@@ -153,13 +174,14 @@ while True:
                      print("Message signed and sent")
                      print()
                    elif (choice == "3"):
-                     # print("Private key is: ")
-                     # print("Public key is: ")
+                     # show the keys
+                     print(f"Private key is: N = {N}, d = {d}")
+                     print(f"Public key is: N = {N}, e = {e}")
                      print()
                    elif (choice == "4"):
-                     # generate a new public key 
-                     # generate a new private key 
-                     # print("New keys have been generated")
+                     # generate the new keys
+                     N, e, d = genKey()
+                     print("New keys have been generated")
                      print()
                    else: 
                      break
